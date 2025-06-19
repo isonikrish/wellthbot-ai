7866756918457ea606d307a0032ba63a53f95e29
Krish Soni <isonikrish@gmail.com>
2025-06-20 02:07:05 +0530

    deployment fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
